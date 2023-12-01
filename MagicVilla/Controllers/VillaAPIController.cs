decdd31bb0f680080956c89294adb77ce664feb2
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-12-01 21:21:03 +0200

    feat: configure first API endpoint

--- MagicVilla/Controllers/VillaAPIController.cs
+++ MagicVilla/Controllers/VillaAPIController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla.Controllers
 {
+    // We need to apply [Route] attribute to API class.
+    [Route("api/[controller]")]
+    // Classes with [ApiController] are configured with features to improve developers experience for buildeing API.
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id = 1, Name = "Pool Villa" },
+                new Villa { Id = 2, Name = "Beach Villa" },
+            };
+        }
     }
 }
