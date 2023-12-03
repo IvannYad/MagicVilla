d57734e60d3da60d284c459d90a85142b2658017
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-12-03 22:20:44 +0200

    feat: add and implement IVillaNumberRepository

---
+++ MagicVilla/Repository/IRepository/IVillaNumberRepository.cs
@@ -0,0 +1,9 @@
+﻿using MagicVilla.Models;
+
+namespace MagicVilla.Repository.IRepository
+{
+    public interface IVillaNumberRepository : IRepository<VillaNumber>
+    {
+        void Update(VillaNumber entity);
+    }
+}
