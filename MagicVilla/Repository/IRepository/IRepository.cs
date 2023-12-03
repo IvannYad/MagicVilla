8f16f51b990b6c1541e5f11f5994ef48bd55dd62
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-12-03 19:49:01 +0200

    feat: implement IRepository

--- MagicVilla/Repository/IRepository/IRepository.cs
+++ MagicVilla/Repository/IRepository/IRepository.cs
@@ -1,10 +1,12 @@
-﻿namespace MagicVilla.Repository.IRepository
+﻿using System.Linq.Expressions;
+
+namespace MagicVilla.Repository.IRepository
 {
     public interface IRepository<T>
     {
-        IEnumerable<T> GetAllAsync();
+        Task<List<T>> GetAllAsync();
         //IEnumerable<T> GetAllAsync(Func<T, bool> filter);
-        T GetAsync(Func<T, bool> filter, bool tracked = false);
+        Task<T?> GetAsync(Expression<Func<T, bool>> filter, bool tracked = false);
         Task CreateAsync(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> enrtites);
