47d45c608755200e7206774879a0fd3bc0c65353
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-12-03 14:11:36 +0200

    feat: add IRepository; add IVillaRepository

---
+++ MagicVilla/Repository/IRepository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace MagicVilla.Repository.IRepository
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAllAsync();
+        //IEnumerable<T> GetAllAsync(Func<T, bool> filter);
+        T GetAsync(Func<T, bool> filter, bool tracked = false);
+        Task CreateAsync(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> enrtites);
+    }
+}
---
+++ MagicVilla/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,9 @@
+﻿using MagicVilla.Models;
+
+namespace MagicVilla.Repository.IRepository
+{
+    public interface IVillaRepository : IRepository<Villa>
+    {
+        void Update(Villa entity);
+    }
+}
