24daefe3c9463f7ab4a576bbf6867975fa64f4e6
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-12-03 21:25:15 +0200

    feat: create VillaNumber model; create VillaNumberDTOs

---
+++ MagicVilla/Models/DTO/VillaNumberDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla.Models.DTO
+{
+    public class VillaNumberDTO
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
+    }
+}
