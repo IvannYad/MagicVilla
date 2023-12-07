ae75ea396b4208ab3755e14cd787a3ddc49e8d07
Ivan Yadchyshyn <yad7devadress@gmail.com>
2023-12-07 21:45:45 +0200

    feat: implement Create Villa UI

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -4,6 +4,7 @@ using MagicVilla_Web.Models.DTO;
 using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.ComponentModel;
 
 namespace MagicVilla_Web.Controllers
 {
@@ -25,6 +26,27 @@ namespace MagicVilla_Web.Controllers
                 villaDTOs = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result)!);
 
             return View(villaDTOs);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateVilla()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("CreateVilla")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateVillaPOST(VillaCreateDTO villaCreateDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _villaService.CreateAsync<APIResponse>(villaCreateDTO);
+                if (response is not null && response.IsSuccess)
+                    return RedirectToAction(nameof(IndexVilla));
+            }
+
+            return View("CreateVilla", villaCreateDTO);
         }
     }
 }
